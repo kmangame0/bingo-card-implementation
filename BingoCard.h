3fb5d3fe91aea4322d783495720ae12d364fbbd2
kmangame0 <kmangame0@gmail.com>
2016-09-29 02:07:15 -0400

    wrong file included in first commit

---
+++ BingoCard.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TOTAL_BALLS_ROW 4
+#define TOTAL_BALLS_COL 4
+
+class BingoCard{
+public:
+	BingoCard();
+	void InitCard();
+	//void ShuffleCard();
+	void PrintCard();
+	int card[TOTAL_BALLS_ROW][TOTAL_BALLS_COL];
+	int minColVal = 1;
+}
