03b5f3a9b8ed413a756ab39c7b209f13c26db914
kmangame0 <kmangame0@gmail.com>
2016-09-29 01:57:00 -0400

    first commit

---
+++ BingoCard.cpp
@@ -0,0 +1,30 @@
+#include "BingoCard.h"
+
+BingoCard::BingoCard(){
+	
+	srand(time(NULL));
+
+}
+
+void BingoCard::InitCard(){
+	for(int i = 0; i <= TOTAL_BALLS_ROW; i++){
+
+		if(i > 0) minColVal = minColVal + 15;
+		
+		for(int j = 0; j <= TOTAL_BALLS_COL; j++){
+			
+			card[i][j] = minColVal
+		
+		}
+	
+	}
+
+}
+
+void BingoCard::PrintCard(){
+	for(int i = 0; i <= 4; i++){
+		for(int j = 0; j <= 4; j++){
+			printf("card[%d][%d] = %d\n", i, j, card[i][j]);
+		}
+	}
+}
